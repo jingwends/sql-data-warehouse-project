d6ca6dac9378c404be896b9c7d2d622136b449f8
Jingwen Chen <109560483+jingwends@users.noreply.github.com>
2025-05-06 18:06:57 -0400

    Update init_databse.sql

--- scipts/init_databse.sql
+++ scipts/init_databse.sql
@@ -1,1 +1,8 @@
+/* Create Databse and Schemas
+Scipt Purpose: 
+  This script creates a new databse named "DataWarehouse" after checking if it already exists. If the databse eexists, it is dropeed and recreated. Additionally, the script sets up three shcemas within the database: "bronze", "silver", and "gold". 
 
+Warning: 
+  Running this script will drop the entire "DataWarehouse" database if it exists. All data in the databse will be permanently deleted. Proceed with caution and ensure you have proper backups before running this script. 
+
+*/
