60f2470bef16f5d16b7ad9fb962a31a0fa41e323
jingwen <jingwenchends@gmail.com>
2025-05-07 18:01:41 -0400

    Table Creation Query update

--- scripts/bronze_crm_creation.sql
+++ scripts/bronze_crm_creation.sql
@@ -1,3 +1,5 @@
+IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_cust_info;
 CREATE TABLE bronze.crm_cust_info(
 cst_id INT,
 cst_key NVARCHAR(50),
@@ -8,6 +10,8 @@ cst_gndr NVARCHAR(50),
 cst_create_date DATE
 );
 
+IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_prd_info;
 CREATE TABLE bronze.crm_prd_info(
 prd_id INT,
 prd_key NVARCHAR (50),
@@ -19,6 +23,8 @@ prd_end_dt DATE
 
 );
 
+IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_sales_details;
 CREATE TABLE bronze.crm_sales_details(
 sls_ord_num NVARCHAR(50),
 sls_prd_key NVARCHAR(50),
