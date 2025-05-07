144087debc48dd33205219abe578abd23f24fd88
jingwen <jingwenchends@gmail.com>
2025-05-06 20:00:48 -0400

    create database and schema

--- scipts/init_databse.sql
+++ scipts/init_databse.sql
@@ -6,3 +6,25 @@ Warning:
   Running this script will drop the entire "DataWarehouse" database if it exists. All data in the databse will be permanently deleted. Proceed with caution and ensure you have proper backups before running this script. 
 
 */
+USE master;
+GO
+
+--Drop and recreate the 'DataWarehouse' databse
+IF EXISTS (SELECT 1 FROM sys.database WHERE name = 'DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWarehouse SET SIGNLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DataWarehouse;
+END;
+GO
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse; 
+
+-- Cretae Schema
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA sliver;
+GO
+CREATE SCHEMA gold;
+GO
